3874b5ff24198c228490a85fc3047f5e4fc1a184
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-09-20 16:10:00 -0400

    Lab 3
    Average, Audiobooks, and Seasons from Lab 03

---
+++ Audiobooks.cs
@@ -0,0 +1,96 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: Audiobooks.cs
+* Project name: Lab3Audiobooks
+* Solution name: Lab3
+*--------------------------------------------------------------------
+* Author’s name and email: Adam Hooven, hoovenar@etsu.edu
+* Course-Section: CSCI 1250
+* Creation Date: 09/20/2023
+* Modified Date: 09/20/2023
+* 
+* 
+*    if (choice == 'A')
+        {
+            if (numBooks > INCLUDED_A_BOOKS)
+            {
+                totalCharges = PACKAGE_A_PRICE + (EXTRA_A_BOOKS * (numBooks - INCLUDED_A_BOOKS));
+            }
+            else
+            {
+                totalCharges = PACKAGE_A_PRICE;
+            }
+        }
+        else if (choice == 'B')
+        {
+            if (numBooks > INCLUDED_B_BOOKS)
+            {
+                totalCharges = PACKAGE_B_PRICE + (EXTRA_B_BOOKS * (numBooks - INCLUDED_B_BOOKS));
+            }
+            else
+            {
+                totalCharges = PACKAGE_B_PRICE;
+            }
+        }
+        else if (choice == 'C')
+        {
+            totalCharges = PACKAGE_C_PRICE;
+        }
+        else
+        {
+            Console.WriteLine("You made an invalid choice");
+        }
+* -------------------------------------------------------------------
+*/
+namespace Lab3Audiobook;
+
+/// <summary>
+/// Audiobook program with 3 different price choices available
+/// Calculates total monthly bill for a user based on the User indicated package chosen
+/// and number of books read
+/// </summary>
+
+class Audiobooks
+{
+    //Prices of each package are constant
+    const double PACKAGE_A_PRICE = 9.95;
+    const double PACKAGE_B_PRICE = 13.95;
+    const double PACKAGE_C_PRICE = 19.95;
+
+    //Number of Books included with subscription.
+    //No constant for C declared as number of Books is unlimited
+    const double INCLUDED_A_BOOKS = 10;
+    const double INCLUDED_B_BOOKS = 20;
+
+    //No C for the same reason as above
+    const double EXTRA_A_BOOKS = 2.0;
+    const double EXTRA_B_BOOKS = 1.0;
+
+    static void Main(string[] args)
+    {
+        char choice;
+        int numBooks;
+        double totalCharges = 0.0;
+
+        do
+        {
+            Console.Write("Enter the letter of the Audiobook package you purchased (A, B, or C): ");    //Prompt user for audiobook package
+            choice = char.ToUpper(char.Parse(Console.ReadLine()));
+            if (!((choice == 'A') || (choice == 'B') || (choice == 'C')))
+            {
+                Console.WriteLine("Invalid Package Choice. Please choose another");
+            }
+        } while (!((choice == 'A') || (choice == 'B') || (choice == 'C')));
+
+        Console.Write("Enter the number of audio books you read: ");
+        numBooks = int.Parse(Console.ReadLine());
+
+        ///<summary>
+        ///Calculates total bill based on package chosen and number of Books read
+        ///for case C since books included is unlimited the price for any number of books read will be the package price
+        ///If choice 
+        ///</summary>
+        
+
+    }
+}
---
+++ Seasons.cs
@@ -0,0 +1,65 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: Seasons.cs
+* Project name: Lab3Seasons
+* Solution name: Lab3
+*--------------------------------------------------------------------
+* Author’s name and email: Adam Hooven, hoovenar@etsu.edu
+* Course-Section: CSCI 1250
+* Creation Date: 09/20/2023
+* Modified Date: 09/20/2023
+* -------------------------------------------------------------------
+*/
+namespace Lab3Seasons;
+
+
+/// <summary>
+/// Class that has 5 (Autumn/Fall) Seasons and their translation to Spanish
+/// Prompts user for a season and then prints its translation out to the user
+/// If a user provided something that is not a season they are notified
+/// Thanks the user for using program
+/// </summary>
+
+class Seasons
+{
+    static void Main(string[] args)
+    {
+        //Variable declarations and pre-defined spanish translations
+        string inputSeason;
+        string translateSpring = "la primavera";
+        string translateSummer = "el Verano";
+        string translateAutumnFall = "el otono";
+        string translateWinter = "el invierno";
+
+        Console.WriteLine("Enter the name of a season and I'll translate it to Spanish");
+        inputSeason = Console.ReadLine();
+
+        ///<summary>
+        /// Converts input string to all uppercase to avoid any issues with all lower or mixed case of a season
+        /// </summary>
+        switch(inputSeason.ToUpper())
+        {
+            case "SPRING":
+                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateSpring}");
+                break;
+            case "SUMMER":
+                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateSummer}");
+                break;
+            case "AUTUMN":
+                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateAutumnFall}");
+                break;
+            case "FALL":
+                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateAutumnFall}");
+                break;
+            case "WINTER":
+                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateWinter}");
+                break;
+            default:
+                Console.WriteLine($"{inputSeason.ToUpper()} is an invalid season.");
+                break;
+        }
+
+        Console.WriteLine("Thank you for using my program!");
+    }
+}
+
