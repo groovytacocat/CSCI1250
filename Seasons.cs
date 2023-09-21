5ed21f9818dc9b119ccba11c81960a1256829d62
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-09-21 15:46:03 -0400

    Update Seasons.cs

--- Seasons.cs
+++ Seasons.cs
@@ -1,4 +1,4 @@
-﻿/**
+/**
 *--------------------------------------------------------------------
 * File name: Seasons.cs
 * Project name: Lab3Seasons
@@ -25,37 +25,38 @@ class Seasons
     static void Main(string[] args)
     {
         //Variable declarations and pre-defined spanish translations
-        string inputSeason;
-        string translateSpring = "la primavera";
-        string translateSummer = "el Verano";
-        string translateAutumnFall = "el otono";
-        string translateWinter = "el invierno";
+        string season;
+        string springSpanish = "la primavera";
+        string summerSpanish = "el Verano";
+        string autumnFallSpanish = "el otono";
+        string winterSpanish = "el invierno";
 
         Console.WriteLine("Enter the name of a season and I'll translate it to Spanish");
-        inputSeason = Console.ReadLine();
+        season = Console.ReadLine();
 
         ///<summary>
         /// Converts input string to all uppercase to avoid any issues with all lower or mixed case of a season
+        /// Default case covers the case of invalid input.
         /// </summary>
-        switch(inputSeason.ToUpper())
+        switch(season.ToUpper())
         {
             case "SPRING":
-                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateSpring}");
+                Console.WriteLine($"{season.ToUpper()} in Spanish is {springSpanish}");
                 break;
             case "SUMMER":
-                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateSummer}");
+                Console.WriteLine($"{season.ToUpper()} in Spanish is {summerSpanish}");
                 break;
             case "AUTUMN":
-                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateAutumnFall}");
+                Console.WriteLine($"{season.ToUpper()} in Spanish is {autumnFallSpanish}");
                 break;
             case "FALL":
-                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateAutumnFall}");
+                Console.WriteLine($"{season.ToUpper()} in Spanish is {autumnFallSpanish}");
                 break;
             case "WINTER":
-                Console.WriteLine($"{inputSeason.ToUpper()} in Spanish is {translateWinter}");
+                Console.WriteLine($"{season.ToUpper()} in Spanish is {winterSpanish}");
                 break;
             default:
-                Console.WriteLine($"{inputSeason.ToUpper()} is an invalid season.");
+                Console.WriteLine($"{season.ToUpper()} is an invalid season.");
                 break;
         }
 
