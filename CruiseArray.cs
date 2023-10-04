9841f44c3cf8ea9d3e61e383385bf1bced471d25
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-10-04 15:01:16 -0400

    Adding Lab5 Cruise

---
+++ CruiseArray.cs
@@ -0,0 +1,87 @@
+﻿/**
+*--------------------------------------------------------------------
+* File name: CruiseArray.cs
+* Project name: CruiseArray
+* Solution name: Lab 5
+*--------------------------------------------------------------------
+* Author’s name and email: Adam Hooven, hoovenar@etsu.edu
+* Course-Section: CSCI 1250
+* Creation Date: 10/4/2023
+* Modified Date: 10/4/2023
+* -------------------------------------------------------------------
+*/
+
+namespace CruiseArray;
+
+
+class CruiseArray
+{
+    static void Main(string[] args)
+    {
+        int numCruises;
+        int minIndex;
+        int maxIndex;
+        double averagePrice;
+        double maxPrice;
+        double minPrice;
+
+        averagePrice = 0;
+        minIndex = 0;
+        maxIndex = 0;
+        maxPrice = 0;
+        minPrice = 0.0;
+
+        Console.WriteLine("Welcome to the Cruise Price Checker!!\n");
+
+        Console.Write("How many cruises are you price checking? ");
+        numCruises = int.Parse(Console.ReadLine());
+
+        double[] cruisePrices = new double[numCruises];
+        string[] cruiseLocation = new string[numCruises];
+
+
+        ///<summary>
+        /// Prompts user to give a location for a cruise, then prompts user for price of that cruise
+        /// Sets maximum price and minimum price to be the value of the first element in the price array
+        /// Compares current value in array to the "Max" value if current index of array is greater/equal price Max is set to that value and maxIndex is assigned the current index
+        /// If current price is not greater than Max compares the value to the min if current price is less/equal to min Min gets that value and minIndex gets that index
+        /// If not greater than max or less than min program just continues to iterate until array
+        /// Also uses averagePrice to store a running total of all costs
+        ///</summary>
+        for(int i = 0; i < numCruises; i++)
+        {
+            Console.Write($"\nEnter the location of Cruise {i+1}: ");
+            cruiseLocation[i] = Console.ReadLine();
+
+            Console.Write($"Enter the price of the Cruise {i+1}: $");
+            cruisePrices[i] = double.Parse(Console.ReadLine());
+
+            averagePrice += cruisePrices[i];
+
+            if(i == 0)
+            {
+                maxPrice = minPrice = cruisePrices[i];
+            }
+            
+            if (cruisePrices[i] >= maxPrice)
+            {
+                maxPrice = cruisePrices[i];
+                maxIndex = i;
+            }
+            else if(cruisePrices[i] <= minPrice)
+            {
+                minPrice = cruisePrices[i];
+                minIndex = i;
+            }
+        }
+
+        averagePrice /= (double)numCruises;     //Takes total sum of all cruises and divides by number of cruises (cast to double due to being integer) to get average
+
+        Console.WriteLine($"\nMost Expensive Cruise:\n\t{cruiseLocation[maxIndex]}\n\t{maxPrice:C2}");
+
+        Console.WriteLine($"\nLeast Expensive Cruise:\n\t{cruiseLocation[minIndex]}\n\t{minPrice:C2}");
+
+        Console.WriteLine($"\nAverage Cruise Price: {averagePrice:C2}");
+
+    }
+}
---
+++ CruiseList.cs
@@ -0,0 +1,103 @@
+﻿
+
+using System;
+/**
+*--------------------------------------------------------------------
+* File name: CruiseList.cs
+* Project name: CruiseList
+* Solution name: Lab 5
+*--------------------------------------------------------------------
+* Author’s name and email: Adam Hooven, hoovenar@etsu.edu
+* Course-Section: CSCI 1250
+* Creation Date: 10/4/2023
+* Modified Date: 10/4/2023
+* -------------------------------------------------------------------
+*/
+
+namespace CruiseList;
+
+
+/*
+ 
+ Create the same program above except with a List with a few differences:
+ Don’t ask the user how many cruises at the beginning of the program. Instead, ask the user if there is another
+cruise to add. Continue adding cruise locations and prices until the user doesn’t have more to add.
+ 
+ 
+ */
+class CruiseList
+{
+    static void Main(string[] args)
+    {
+        List<string> cruiseLocation = new List<string>();
+        List<double> cruisePrices = new List<double>();
+
+        double maxPrice;
+        double minPrice;
+        double averagePrice;
+        int maxIndex;
+        int minIndex;
+        char userChoice;
+
+        averagePrice = 0.0;
+
+        maxIndex = 0;
+        minIndex = 0;
+
+        Console.WriteLine("Welcome to the Cruise Price Checker!! (Now with a Lists)\n");
+
+        ///<summary>
+        /// Prompts user to enter a cruise location and a price
+        /// After getting values from user asks if user wishes to add another
+        /// If user inputs 'n' or "N" ends loading the cruise price/location
+        ///</summary>
+        do
+        {
+            Console.Write("Enter a Cruise Location: ");
+            cruiseLocation.Add(Console.ReadLine());
+
+            Console.Write("Enter the price of the cruise: $");
+            cruisePrices.Add(double.Parse(Console.ReadLine()));
+
+            Console.Write("\nWould you like to add another cruise? ");
+            userChoice = char.ToUpper(char.Parse(Console.ReadLine()));
+            Console.WriteLine();
+
+        } while (userChoice != 'N');
+
+        //Set Max and Min to first element array before comparing values
+        maxPrice = cruisePrices[0];
+        minPrice = cruisePrices[0];
+
+
+        ///<summary>
+        /// Iterates through Cruise Prices and stores running total in Average
+        /// Compares current price value to the max, if current price is greater then maxPrice is set to the current price
+        /// If current value isn't greater then checks if it is less than minPrice if so minPrice is set to current value
+        ///</summary>
+        for(int i = 0; i < cruisePrices.Count; i++)
+        {
+            averagePrice += cruisePrices[i];
+
+            if (cruisePrices[i] >= maxPrice)
+            {
+                maxPrice = cruisePrices[i];
+                maxIndex = i;
+            }
+            else if (cruisePrices[i] <= minPrice)
+            {
+                minPrice = cruisePrices[i];
+                minIndex = i;
+            }
+        }
+
+        averagePrice /= (double)cruisePrices.Count;
+
+        Console.WriteLine($"\nMost Expensive Cruise:\n\t{cruiseLocation[maxIndex]}\n\t{maxPrice:C2}");
+
+        Console.WriteLine($"\nLeast Expensive Cruise:\n\t{cruiseLocation[minIndex]}\n\t{minPrice:C2}");
+
+        Console.WriteLine($"\nAverage Cruise Price: {averagePrice:C2}");
+    }
+}
+
