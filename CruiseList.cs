fd383db74da06197ee9703a0d7d5c1efa8d5e385
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-10-09 19:39:27 -0400

    Update CruiseList.cs

--- CruiseList.cs
+++ CruiseList.cs
@@ -1,6 +1,3 @@
-
-
-using System;
 /**
 *--------------------------------------------------------------------
 * File name: CruiseList.cs
@@ -10,11 +7,15 @@ using System;
 * Author’s name and email: Adam Hooven, hoovenar@etsu.edu
 * Course-Section: CSCI 1250
 * Creation Date: 10/4/2023
-* Modified Date: 10/4/2023
+* Modified Date: 10/9/2023
 * -------------------------------------------------------------------
 */
 
+using System;
+using MyDLL;
+
 namespace CruiseList;
+
 class CruiseList
 {
     static void Main(string[] args)
@@ -25,35 +26,42 @@ class CruiseList
         double averagePrice = 0.0;
         int maxIndex = 0;
         int minIndex = 0;
-        char userChoice;
+        double price;
 
-        Console.WriteLine("Welcome to the Cruise Price Checker!! (Now with a Lists)\n");
+        Console.WriteLine("Welcome to the Cruise Price Checker!! (Now with a Lists)");
 
         ///<summary>
         /// Prompts user to enter a cruise location and a price
+        /// If the price is not a valid double or if price is negative notify user that input is invalid and re-prompt for input
         /// After getting values from user asks if user wishes to add another
         /// If user inputs 'n' or "N" ends loading the cruise price/location
         ///</summary>
         do
         {
-            Console.Write("Enter a Cruise Location: ");
+            Console.Write("\nEnter a Cruise Location: ");
             cruiseLocation.Add(Console.ReadLine());
 
-            Console.Write("Enter the price of the cruise: $");
-            cruisePrices.Add(double.Parse(Console.ReadLine()));
+            do
+            {
+                price = CSCI1250.Validate<double>("Enter the price of the cruise: $", "\nPlease enter a valid double\n");
+                if(price < 0)
+                {
+                    Console.WriteLine("\nPlease enter a non-negative value\n");
+                }
+            } while (price < 0);
 
-            Console.Write("\nWould you like to add another cruise? ");
-            userChoice = char.ToUpper(char.Parse(Console.ReadLine()));
+            cruisePrices.Add(price);
+
             Console.WriteLine();
 
-        } while (userChoice != 'N');
+        } while (CSCI1250.Repeat("Would you like to add another cruise? ", "\nPlease enter Y to continue or N to stop\n"));
 
         ///<summary>
         /// Iterates through Cruise Prices and stores running total in Average
         /// Compares current price value to that of the value at the index of maxIndex if it is greater then maxIndex gets the index of current price
         /// Same as above but for the minimum case
         ///</summary>
-        for(int i = 0; i < cruisePrices.Count; i++)
+        for (int i = 0; i < cruisePrices.Count; i++)
         {
             averagePrice += cruisePrices[i];
 
@@ -67,8 +75,9 @@ class CruiseList
             }
         }
 
-        averagePrice /= (double)cruisePrices.Count; //Takes running total calculated from above and gets average by dividing by the number of cruises (cast to a double to avoid truncation of the decimal)
+        averagePrice /= cruisePrices.Count; //Takes running total calculated from above and gets average by dividing by the number of cruises (cast to a double to avoid truncation of the decimal)
 
+        // Format to print the price as a currency to include $ a ',' if applicable and rounded to 2 decimal places
         Console.WriteLine($"\nMost Expensive Cruise:\n\t{cruiseLocation[maxIndex]}\n\t{cruisePrices[maxIndex]:C}");
 
         Console.WriteLine($"\nLeast Expensive Cruise:\n\t{cruiseLocation[minIndex]}\n\t{cruisePrices[minIndex]:C}");
@@ -76,4 +85,3 @@ class CruiseList
         Console.WriteLine($"\nAverage Cruise Price: {averagePrice:C}");
     }
 }
-
