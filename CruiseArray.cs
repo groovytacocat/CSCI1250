fd5b1b0475be4454aea11128ab50b30cc506d2dd
groovytacocat <71411552+groovytacocat@users.noreply.github.com>
2023-10-09 19:40:04 -0400

    Update CruiseArray.cs

--- CruiseArray.cs
+++ CruiseArray.cs
@@ -7,10 +7,11 @@
 * Author’s name and email: Adam Hooven, hoovenar@etsu.edu
 * Course-Section: CSCI 1250
 * Creation Date: 10/4/2023
-* Modified Date: 10/4/2023
+* Modified Date: 10/9/2023
 * -------------------------------------------------------------------
 */
 
+using MyDLL;
 namespace CruiseArray;
 
 
@@ -25,8 +26,22 @@ class CruiseArray
 
         Console.WriteLine("Welcome to the Cruise Price Checker!!\n");
 
-        Console.Write("How many cruises are you price checking? ");
-        numCruises = int.Parse(Console.ReadLine());
+
+        ///<summary>
+        /// Prompts user to input number of cruises they wish to price
+        /// Ensures user input is not only a valid integer but also a non-negative integer
+        /// Notifies if the user's input is invalid and prompts them for correct values
+        ///</summary>
+        do
+        {
+            numCruises = CSCI1250.Validate<int>("How many cruises are you price checking? ", "\nPlease enter a valid integer\n");
+
+            if(numCruises < 0)
+            {
+                Console.WriteLine("\nPlease enter a non-negative value\n");
+            }
+
+        } while (numCruises < 0);
 
         double[] cruisePrices = new double[numCruises];
         string[] cruiseLocation = new string[numCruises];
@@ -34,18 +49,27 @@ class CruiseArray
 
         ///<summary>
         /// Prompts user to give a location for a cruise, then prompts user for price of that cruise
+        /// If the price is not a valid double or the value input is negative User is notified that their input is invalid and prompts for new input
         /// Calculates a running total of Cruise prices as they are entered
         /// Stores index of the highest price and the lowest price
         /// Compares the current price value to the value at maxIndex if current is greater maxIndex is set to current index
         /// Same as above however if current price is less than value at minIndex minIndex is set to current index
         ///</summary>
-        for(int i = 0; i < numCruises; i++)
+        for (int i = 0; i < numCruises; i++)
         {
-            Console.Write($"\nEnter the location of Cruise {i+1}: ");
+            Console.Write($"\nEnter the location of Cruise {i + 1}: ");
             cruiseLocation[i] = Console.ReadLine();
 
-            Console.Write($"Enter the price of the Cruise {i+1}: $");
-            cruisePrices[i] = double.Parse(Console.ReadLine());
+            do
+            {
+                cruisePrices[i] = CSCI1250.Validate<double>($"Enter the price of the Cruise {i + 1}: $", "\nPlease enter a valid double\n");
+
+                if (cruisePrices[i] < 0)
+                {
+                    Console.WriteLine("\nPlease enter a non-negative value\n");
+                }
+
+            } while (cruisePrices[i] < 0);
 
             averagePrice += cruisePrices[i];
 
@@ -59,9 +83,10 @@ class CruiseArray
             }
         }
 
-        averagePrice /= (double)numCruises;     //Takes total sum of all cruises and divides by number of cruises (cast to double due to being integer) to get average
+        averagePrice /= numCruises;     //Takes total sum of all cruises and divides by number of cruises
 
-        Console.WriteLine($"\nMost Expensive Cruise:\n\t{cruiseLocation[maxIndex]}\n\t{cruisePrices[maxIndex]:C}"); //Format to print the price as a currency to include $ a ',' if applicable and rounded to 2 decimal places
+        // Format to print the price as a currency to include $ a ',' if applicable and rounded to 2 decimal places
+        Console.WriteLine($"\nMost Expensive Cruise:\n\t{cruiseLocation[maxIndex]}\n\t{cruisePrices[maxIndex]:C}"); 
 
         Console.WriteLine($"\nLeast Expensive Cruise:\n\t{cruiseLocation[minIndex]}\n\t{cruisePrices[minIndex]:C}");
 
